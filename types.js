f34d3f969fb077bcb4da9141505c11048481817d
Mohammed Alhaddar <moalhaddar@gmail.com>
2024-08-16 10:45:13 +0300

    Create dist folder

--- types.js
+++
@@ -1,7 +0,0 @@
-export var MouseButton;
-(function (MouseButton) {
-    MouseButton[MouseButton["Left"] = 0] = "Left";
-    MouseButton[MouseButton["Middle"] = 1] = "Middle";
-    MouseButton[MouseButton["Right"] = 2] = "Right";
-})(MouseButton || (MouseButton = {}));
-//# sourceMappingURL=types.js.map
